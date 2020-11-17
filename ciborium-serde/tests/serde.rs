e5d3b062ad6d793486700d954b8faa211942b3e8
Nathaniel McCallum <npmccallum@redhat.com>
2020-11-17 18:05:57 -0500

    Validate how Rust data types are encoded
    In the serde test, we validate that they encode and decode successfully.
    But we now additionally validate the structure of the encoded types. For
    example, we validate that enums are encoded as maps with the name of the
    field as the key.
    
    Signed-off-by: Nathaniel McCallum <npmccallum@redhat.com>

--- ciborium-serde/tests/serde.rs
+++ ciborium-serde/tests/serde.rs
@@ -3,6 +3,7 @@
 extern crate alloc;
 
 use ciborium_serde::{
+    cbor,
     de::from_reader,
     ser::into_writer,
     value::{Bytes, Value},
@@ -25,63 +26,71 @@ struct Newtype(u8);
 
 #[derive(Deserialize, Serialize, Copy, Clone, Debug, PartialEq, Eq)]
 enum Enum {
-    UnitVariant,
-    NewtypeVariant(u8),
-    TupleVariant(u8, u16),
-    StructVariant { first: u8, second: u16 },
+    Unit,
+    Newtype(u8),
+    Tuple(u8, u16),
+    Struct { first: u8, second: u16 },
 }
 
-#[rstest(item,
-    case(true),
-    case(false),
+#[rstest(item, value,
+    case(true, cbor!(true).unwrap()),
+    case(false, cbor!(false).unwrap()),
 
-    case(7u8),
-    case(7u16),
-    case(7u32),
-    case(7u64),
-    case(7u128),
+    case(7u8, cbor!(7).unwrap()),
+    case(7u16, cbor!(7).unwrap()),
+    case(7u32, cbor!(7).unwrap()),
+    case(7u64, cbor!(7).unwrap()),
+    case(7u128, cbor!(7).unwrap()),
 
-    case(7i8),
-    case(-7i8),
-    case(7i16),
-    case(-7i16),
-    case(7i32),
-    case(-7i32),
-    case(7i64),
-    case(-7i64),
-    case(7i128),
-    case(-7i128),
+    case(7i8, cbor!(7).unwrap()),
+    case(-7i8, cbor!(-7).unwrap()),
+    case(7i16, cbor!(7).unwrap()),
+    case(-7i16, cbor!(-7).unwrap()),
+    case(7i32, cbor!(7).unwrap()),
+    case(-7i32, cbor!(-7).unwrap()),
+    case(7i64, cbor!(7).unwrap()),
+    case(-7i64, cbor!(-7).unwrap()),
+    case(7i128, cbor!(7).unwrap()),
+    case(-7i128, cbor!(-7).unwrap()),
 
-    case('e'),
-    case('é'),
-    case("foo".to_string()),
+    case('e', cbor!('e').unwrap()),
+    case('é', cbor!('é').unwrap()),
+    case("foo".to_string(), cbor!("foo").unwrap()),
 
-    case(Bytes::from(&b"\x00\x01\x02\x03"[..])),
+    case(Bytes::from(&b"\x00\x01\x02\x03"[..]), Value::Bytes(vec![0, 1, 2, 3].into())),
 
-    case(Option::<u8>::None),
-    case(Option::<u8>::Some(7)),
+    case(Option::<u8>::None, cbor!(null).unwrap()),
+    case(Option::<u8>::Some(7), cbor!(7).unwrap()),
 
-    case(()),
-    case(UnitStruct),
-    case(Newtype(123)),
+    case((), cbor!(null).unwrap()),
+    case(UnitStruct, cbor!(null).unwrap()),
+    case(Newtype(123), cbor!(123).unwrap()),
 
-    case(vec![1usize, 2, 3]),
-    case((22u8, 23u16)),
-    case(TupleStruct(33, 34)),
+    case(vec![1usize, 2, 3], cbor!([1, 2, 3]).unwrap()),
+    case((22u8, 23u16), cbor!([22, 23]).unwrap()),
+    case(TupleStruct(33, 34), cbor!([33, 34]).unwrap()),
 
     case({
         let mut map = BTreeMap::new();
         map.insert("foo".to_string(), "bar".to_string());
         map.insert("baz".to_string(), "qux".to_string());
         map
-    }),
+    }, cbor!({"baz" => "qux", "foo" => "bar"}).unwrap()),
 
-    case(Enum::UnitVariant),
-    case(Enum::NewtypeVariant(45)),
-    case(Enum::TupleVariant(56, 67)),
-    case(Enum::StructVariant { first: 78, second: 89 }),
+    case(Enum::Unit, cbor!({"Unit" => null}).unwrap()),
+    case(Enum::Newtype(45), cbor!({"Newtype" => 45}).unwrap()),
+    case(Enum::Tuple(56, 67), cbor!({"Tuple"=> vec![56, 67]}).unwrap()),
+    case(Enum::Struct {
+        first: 78,
+        second: 89
+    }, cbor!({
+        "Struct" => {
+            "first" => 78,
+            "second" => 89
+        }
+    }).unwrap()),
 )]
-fn test<'de, T: Serialize + Deserialize<'de> + Debug + Eq>(item: T) {
+fn test<'de, T: Serialize + Deserialize<'de> + Debug + Eq>(item: T, value: Value) {
     // Encoded into/from CBOR
     let mut buf = Vec::new();
     into_writer(&item, &mut buf).unwrap();
@@ -92,6 +101,7 @@ fn test<'de, T: Serialize + Deserialize<'de> + Debug + Eq>(item: T) {
     // Encoded into/from ciborium::serde::value::Value
     let val = Value::serialized(&item).unwrap();
     eprintln!("{:?}", val);
+    assert_eq!(value, val);
     let back: T = val.deserialized().unwrap();
     assert_eq!(item, back);
 }
